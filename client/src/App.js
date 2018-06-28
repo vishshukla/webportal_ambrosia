69ea984fe46b4009b2fb9a348fe7b64dda3acb1a
vishshukla <vishwas.shukla@outlook.com>
2018-06-28 15:59:39 -0700

    added footer,navbar, and landing components. added sample background image as well

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <Navbar />
+        <Landing />
+        <Footer />
       </div>
     );
   }
